40e1b85e433676114acd36c3aa489171321df5aa
Digvijay Desai <127867298+digvijay164@users.noreply.github.com>
2024-10-23 12:41:02 +0100

    imerssion journey final slider

--- immersion-journey/final-slider/javascripts/script.js
+++ immersion-journey/final-slider/javascripts/script.js
@@ -1,46 +1,47 @@
 gsap.registerPlugin(ScrollTrigger);
 
-      var panelsbg = document.getElementsByClassName("post");
-      var panels = document.getElementsByClassName("point");
+var panelsbg = document.getElementsByClassName("post");
+var panels = document.getElementsByClassName("point");
 
-      for (var i = 0; i < panels.length; i++) {
-        panels[i].addEventListener("mouseover", function () {
-          console.log("working");
-          //this.style.fontSize ="2rem"
-          gsap.to(this, {
-            fontSize: "2rem",
-          });
-        });
-        panels[i].addEventListener("mouseout", function () {
-          console.log("working");
-          //this.style.fontSize ="2rem"
-          gsap.to(this, {
-            fontSize: "1rem",
-          });
-        });
+for (var i = 0; i < panels.length; i++) {
+  panels[i].addEventListener("mouseover", function () {
+    console.log("working");
+    //this.style.fontSize ="2rem"
+    gsap.to(this, {
+      fontSize: "2rem",
+    });
+  });
+  panels[i].addEventListener("mouseout", function () {
+    console.log("working");
+    //this.style.fontSize ="2rem"
+    gsap.to(this, {
+      fontSize: "1rem",
+    });
+  });
 
-        gsap.fromTo(
-          panels[i],
-          {
-            // fontSize: "1.5",
-          },
-          {
-            fontSize: "2rem",
-            scrollTrigger: {
-              trigger: panels[i],
-              start: "top 1vh",
-              end: "bottom center",
-              toggleActions: "play none none reverse",
-            },
-          }
-        );
-      }
+  gsap.fromTo(
+    panels[i],
+    {
+      // fontSize: "1.5",
+    },
+    {
+      fontSize: "2rem",
+      scrollTrigger: {
+        trigger: panels[i],
+        start: "top 1vh",
+        end: "bottom center",
+        toggleActions: "play none none reverse",
+      },
+    }
+  );
+}
 
-      gsap.to('.icon', {
-        y: index * window.innerHeight, // Adjust the calculation based on your layout
-        scrollTrigger: {
-            trigger: section,
-            start: 'top center',
-            end: 'bottom center',
-            scrub: true, // Smooth animation on scroll
-        }});
+gsap.to(".icon", {
+  y: index * window.innerHeight, // Adjust the calculation based on your layout
+  scrollTrigger: {
+    trigger: section,
+    start: "top center",
+    end: "bottom center",
+    scrub: true, // Smooth animation on scroll
+  },
+});
